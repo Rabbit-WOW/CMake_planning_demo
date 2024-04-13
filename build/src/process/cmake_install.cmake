953f32d689df4268a9f9ca2632d85cbba8d5ec46
Jiaqi Wang <767325222@qq.com>
2024-04-13 15:31:44 +0800

    version:2

--- build/src/process/cmake_install.cmake
+++ build/src/process/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Code/C++/Cmake/CMake_planning_demo
+# Install script for directory: D:/Code/C++/Cmake/CMake_planning_demo/src/process
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,18 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "D:/progrom files/IDE Support library/mingw64/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("D:/Code/C++/Cmake/CMake_planning_demo/build/src/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Code/C++/Cmake/CMake_planning_demo/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
